9725d58ad07660dd509beb3087dd1b7326c8fe5a
Joey Lee <skkshu@gmail.com>
2020-05-14 11:27:49 +0800

    --no-systray

--- dwm-additional-functionality/sck-tog.sh
+++ dwm-additional-functionality/sck-tog.sh
@@ -2,8 +2,8 @@
 
 result=$(ps ax|grep -v grep|grep screenkey)
 if [ "$result" == "" ]; then
-  #eval "screenkey --bg-color white --font-color black &"
-  eval "screenkey &"
+  # eval "screenkey --font-size medium --no-systray --bg-color white --font-color black --font SauceCodePro --opacity 0.3 --timeout 2 &"
+  eval "screenkey --key-mode composed --font-size medium --no-systray --timeout 2 &"
 else
   eval "killall screenkey"
 fi
